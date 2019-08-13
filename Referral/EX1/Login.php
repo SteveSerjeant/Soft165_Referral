79144ef54f5cbed74f3d65eea69ef723301d35cb
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2019-08-13 19:25:59 +0100

    Delete Login.php

--- Referral/EX1/Login.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="public/process_login.php" id="UserLogin" class="form-inline my-2 my-lg-o">
-    <input type="text" id="form_username" placeholder="Username.."minlength="1" name="form-username" class="frm-control mr-sm-2"/>
-    <input type="password" id="form-password" placeholder="Password.." minlength="1" name="form-password" class="frm-control mr-sm-2"/>  
-    <button type="submit" name="login" class="btn btn-info">Sign In</button>
-</form>
-    
-
-
